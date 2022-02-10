766267d36fbaa0d7cd83174e1af29ddeafb6a11e
kavithareddy79 <91481236+KavithaReddy79@users.noreply.github.com>
2022-02-10 16:21:37 +0530

    add semi

--- test/specs/home.js
+++ test/specs/home.js
@@ -1,7 +1,7 @@
 describe('home',()=>{
     it('teest1',async()=>{
         await browser.url('https://practice.automationbro.com')
-        await expect(browser).toHaveTitle('Practice E-Commerce Site – Automation Bro')
+        await expect(browser).toHaveTitle('Practice E-Commerce Site – Automation Bro');
       
 
     })
