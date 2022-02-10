2c6490cf1dfa7bf310415fcc39415479e0fb8320
kavithareddy79 <91481236+KavithaReddy79@users.noreply.github.com>
2022-02-10 15:13:56 +0530

    Inital Commit WDIO project

---
+++ test/specs/home.js
@@ -0,0 +1,8 @@
+describe('home',()=>{
+    it('teest1',async()=>{
+        await browser.url('https://practice.automationbro.com')
+        await expect(browser).toHaveTitle('Practice E-Commerce Site – Automation Bro')
+      
+
+    })
+})
