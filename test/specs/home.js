3793cdf152d43819c80ea515c39d7f46732a9e20
KavithaReddy79 <91481236+KavithaReddy79@users.noreply.github.com>
2022-02-10 16:23:38 +0530

    Update home.js

--- test/specs/home.js
+++ test/specs/home.js
@@ -1,6 +1,6 @@
 describe('home',()=>{
     it('teest1',async()=>{
-        await browser.url('https://practice.automationbro.com')
+        await browser.url('https://practice.automationbro.com');
         await expect(browser).toHaveTitle('Practice E-Commerce Site – Automation Bro');
       
 
